6ef015f9653e898ff6e13cd42f419c99dfd84275
youvalg <yuvalgeva77@gmail.com>
2018-11-24 23:11:20 +0200

    shadows in process

--- glfw2D/glfw2D/res/shaders/basicShader.glsl
+++ glfw2D/glfw2D/res/shaders/basicShader.glsl
@@ -1,7 +1,7 @@
  #version 130 
 
-#define exponent 10
-#define MAX_FLOAT 1000.0
+#define exponent 15
+#define INFINITY 1000.5693
 uniform vec4 eye;
 uniform vec4 ambient;
 uniform vec4[20] objects;
@@ -27,9 +27,10 @@ vec3 calcq0( vec4 plane);
 vec3 calc_light(vec3 p, int obj_idx ,int light_src_idx);
 vec3 calc_spotlight(vec3 p, int obj_idx ,int light_src_idx);
 vec3 calc_directional_light(vec3 p, int obj_idx ,int light_src_idx);
-vec3 get_spolight_position(vec3 p, int light_src_idx);
+vec4 get_spolight_position(int light_src_idx);
 vec3 norm_at_point(Intersection intrsc);
 vec3 calc_R(vec3 N, vec3 L);
+bool not_occluded(vec3 p, int light_idx);
 
 
 
@@ -37,10 +38,10 @@ Intersection findIntersection(vec3 sourcePoint,vec3 v)
 {
 	Intersection ans;
 	float t = 0;
-	ans.t = -1.0;
+	ans.t = INFINITY;
 	for(int i = 0; i < sizes[0]; i++){
 		t = intersect(sourcePoint, v, objects[i]);
-		if(ans.t == -1.0 || t < ans.t){
+		if(t < ans.t){
 			ans.t=t;
 			ans.index = i;
 		}
@@ -52,7 +53,7 @@ Intersection findIntersection(vec3 sourcePoint,vec3 v)
 
 float intersect(vec3 sourcePoint,vec3 v, vec4 object)
 {
-	float t = -1.0;
+	float t = INFINITY;
 	if(object.w < 0.0){
 		t = intersects_plane(sourcePoint,v, object);
 	}else{
@@ -63,12 +64,15 @@ float intersect(vec3 sourcePoint,vec3 v, vec4 object)
 
 float intersects_plane(vec3 sourcePoint,vec3 v, vec4 plane)
 {
-	float t = -1.0;
+	float t = INFINITY;
 	vec3 Q0=calcq0(plane);
 	vec3 N=normalize(plane.xyz);
 	vec3 V=normalize(v);
 	vec3 PQ=Q0-sourcePoint;
 	t=dot(N, PQ/dot(N,V));
+	if(t<=0.0){
+		t = INFINITY;
+	}
    return t; 
 }
 vec3 calcq0( vec4 plane)
@@ -99,7 +103,7 @@ vec3 calcq0( vec4 plane)
 
 float intersects_sphere(vec3 sourcePoint,vec3 v, vec4 sphere)
 {
-	float t = -1.0;
+	float t = INFINITY;
 	vec3 V=normalize(v);
 	vec3 P=sourcePoint;
 	vec3 O=sphere.xyz;
@@ -116,6 +120,9 @@ float intersects_sphere(vec3 sourcePoint,vec3 v, vec4 sphere)
 	if (t<0){
 		t=max(t1,t2);
 	}
+	if(t <=0.0){
+		t =INFINITY;	
+	}
    return t; 
 }
 
@@ -130,7 +137,7 @@ vec3 calc_light(vec3 p, int obj_idx ,int light_src_idx)
 	return ans;
 }
 vec3 calc_spotlight(vec3 p, int obj_idx ,int light_src_idx){
-	vec3 light_position = get_spolight_position(p, light_src_idx);
+	vec3 light_position = get_spolight_position(light_src_idx).xyz;
 	vec3 D = normalize(lightsDirection[light_src_idx].xyz);
 	vec3 L = normalize(p - light_position);
 	vec3 I0 = lightsIntensity[light_src_idx].xyz;
@@ -141,7 +148,7 @@ vec3 calc_directional_light(vec3 p, int obj_idx ,int light_src_idx){
 	return lightsIntensity[light_src_idx];
 }
 
-vec3 get_spolight_position(vec3 p, int light_src_idx){
+vec4 get_spolight_position(int light_src_idx){
 	vec3 ans;
 	int count = -1;
 	for(int i = 0; i <= light_src_idx; i++){
@@ -165,7 +172,31 @@ vec3 norm_at_point(Intersection intrsc)
 
 vec3 calc_R(vec3 N, vec3 L)
 {
-	return reflect(L, N);
+	return reflect(-L, N);
+}
+
+bool not_occluded(vec3 p, int light_idx){
+	vec4 light_src = lightsDirection[light_idx];
+	vec3 v = -normalize(light_src.xyz);
+	Intersection intr = findIntersection(p, v);
+	if(light_src.w == 1.0){
+		vec4 light_pos = get_spolight_position(light_idx);
+		vec3 L = normalize(p - light_pos.xyz);
+		vec3 D = normalize(lightsDirection[light_idx].xyz);
+		if(dot(D,L) < light_pos.w){
+			return false;
+		}
+		float length_to_light = length(light_pos.xyz - p);
+		float length_to_obj = length(intr.p - p);
+		if(intr.t != INFINITY && (length_to_obj < length_to_light)){
+			return false;
+		}
+	}else{
+		if(intr.t != INFINITY ){
+			return false;
+		}
+	}
+	return true;
 }
 
 vec3 colorCalc( Intersection intrsc)
@@ -195,16 +226,18 @@ vec3 colorCalc( Intersection intrsc)
 	}
 	vec3 Ks = vec3(0.7,0.7,0.7);
 	for(int i = 0; i < sizes[1]; i++){
-		vec3 N = norm_at_point(intrsc);
-		vec3 L = lightsDirection[i].xyz;
-		vec3 Ili = calc_light(intrsc.p, intrsc.index, i);
-		if((dot(N,L))>0){
-			diffuse += Kd*(dot(N,L))*Ili;
-		}
-		vec3 V = normalize(eye.xyz - intrsc.p);
-		vec3 R = calc_R(N,L);
-		if((dot(V,R))>0){
-			specular += Ks*(pow(dot(V,R),exponent))*Ili;
+		if(not_occluded(intrsc.p, i)){
+			vec3 N = norm_at_point(intrsc);
+			vec3 L = -lightsDirection[i].xyz;
+			vec3 Ili = calc_light(intrsc.p, intrsc.index, i);
+			if((dot(N,L))>0){
+				diffuse += Kd*(dot(N,L))*Ili;
+			}
+			vec3 V = normalize(eye.xyz - intrsc.p);
+			vec3 R = calc_R(N,L);
+			if((dot(V,R))>0){
+				specular += Ks*(pow(dot(V,R),exponent))*Ili;
+			}
 		}
 	}
 	color = KaIamb + diffuse + specular;
